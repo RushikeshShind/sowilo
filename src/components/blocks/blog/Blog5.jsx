6a7d037796eeb35f230a5eb93f531db12d0ab33e
RushikeshShind <Rushikeshshinde57323@gmail.com>
2024-12-16 12:24:33 +0530

    update

--- src/components/blocks/blog/Blog5.jsx
+++ src/components/blocks/blog/Blog5.jsx
@@ -1,98 +1,60 @@
+import dayjs from 'dayjs';
+import Link from 'next/link';
+// -------- custom components -------- //
+import Carousel from 'components/reuseable/Carousel';
+import NextLink from 'components/reuseable/links/NextLink';
+import carouselBreakpoints from 'utils/carouselBreakpoints'; // -------- data -------- //
 import ContactForm from 'components/common/ContactForm';
 import dynamic from 'next/dynamic';
-import Slider from 'react-slick'; // Import react-slick
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Image from 'next/image';
 
 // Dynamically import Plyr with ssr: false
 const Plyr = dynamic(() => import('plyr-react'), { ssr: false });
 
-const Blog5 = () => {
-  // Slider settings
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    arrows: true,
-  };
+ 
 
+const Blog5 = () => {
   return (
     <section className="wrapper bg-soft-primary">
-      {/* Registration Section */}
       <div className="row">
         <div className="col-lg-10 offset-lg-1 col-xl-8 offset-xl-2 mb-12">
-          <h1
-            className="col-lg-12 display-1 text-center"
-            style={{ fontFamily: 'Sacramento', fontWeight: '500' }}
-          >
+          <h1 className="col-lg-12 display-1 text-center" style={{fontFamily: 'Sacramento', fontWeight: '500px'}}>
             Form for Registration
           </h1>
+          <p className="lead text-center mb-12"></p>
           <ContactForm />
         </div>
       </div>
 
-      {/* Testimonial Slider */}
       <div className="row text-justify">
         <div className="col-xl-9 mx-auto md-10 mb-12">
-          <h1 className="text-center mb-12">Bodynamic Training Testimonials</h1>
-
-          {/* Slider UI */}
-          <Slider {...sliderSettings}>
-            {/* Slide 1 */}
-            <div className="testimonial-card bgcolor-black" >
-              <div className="card p-3 text-center "style={{color:'#134B70' , Width: '500px'}} >
-               
-                <p className="lead mb-4">
-                
-                </p>
-                <Plyr
-            options={{ loadSprite: true, clickToPlay: true }}
+          <h1 className="text-center mb-12">What they're saying about the course</h1>
+          <Plyr 
+            options={{
+              loadSprite: true,
+              clickToPlay: true
+            }}
             source={{
               type: 'video',
-              sources: [
-                {
-                  src: 'https://vimeo.com/469964414?autoplay=1&muted=1',
-                  provider: 'vimeo',
-                },
-              ],
+              sources: [{
+                src: 'https://vimeo.com/469964414?autoplay=1&muted=1&stream_id=Y2xpcHN8NjI0MDMzNDd8aWQ6ZGVzY3xbXQ%3D%3D',
+                provider: 'vimeo'
+              }]
             }}
           />
-              </div>
-            </div>
-
-            {/* Slide 2 (repeat with different content) */}
-            <div className="testimonial-card ">
-              <div className="card p-4 text-center " style={{color:'rgb(247, 170, 129)', Width: '500px'}}>
-           
-                <Plyr
-            options={{ loadSprite: true, clickToPlay: true }}
+          <h3 className="col-xl-12 mb-4 mt-6 text-center">Bodynamic Training Testimonials</h3>
+          <Plyr 
+            options={{
+              loadSprite: true,
+              clickToPlay: true
+            }}
             source={{
               type: 'video',
-              sources: [
-                {
-                  src: 'https://vimeo.com/469964414?autoplay=1&muted=1',
-                  provider: 'vimeo',
-                  
-                },
-              ],
+              sources: [{
+                src: 'https://vimeo.com/276290101?share=copy#t=0',
+                provider: 'vimeo'
+              }]
             }}
           />
-              </div>
-            </div>
-          </Slider>
-        </div>
-      </div>
-
-      {/* Video Testimonials */}
-      <div className="row text-justify mt-12">
-        <div className="col-xl-9 mx-auto md-10 mb-12">
-          
-         
         </div>
       </div>
     </section>
