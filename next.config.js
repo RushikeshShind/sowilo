078bd041fc965e76db07fc3f3a420f7601d7e2ba
rushikesh bapusaheb shinde <114944982+RushikeshShind@users.noreply.github.com>
2025-01-16 15:27:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // swcMinify: true,
-  reactStrictMode: false
-  // output: 'export'
-  // images: {
-  //   unoptimized: true
-  // }
+  swcMinify: true,
+  reactStrictMode: false,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
