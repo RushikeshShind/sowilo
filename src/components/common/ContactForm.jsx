2482b556306d11c01feac7287609b0c8964abd15
RushikeshShind <Rushikeshshinde57323@gmail.com>
2025-01-18 00:01:31 +0530

    email js update

--- src/components/common/ContactForm.jsx
+++ src/components/common/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
